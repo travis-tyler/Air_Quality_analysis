3c5f512988a8bedcc7db4d5eb97aefd91eedf123
hal-pemulis <travisatyler@gmail.com>
2020-06-11 01:37:26 -0500

    Fixed plotly code in app.js

--- 04_Deploy/static/app.js
+++ 04_Deploy/static/app.js
@@ -10,26 +10,37 @@ function buildPlot() {
 
 
         // Trace for 2020 data
-        const trace2020 = [{
+        const trace2020 = {
             x: response.date,
             y: response.aqi_2020,
-            type:"line"
-        }];
+            type:"line",
+            name:"2020 AQI"
+        };
 
         // Trace for 5y data
-        const trace5y = [{
+        const trace5y = {
             x: response.date,
             y: response.avg_5y,
-            type:"line"
-        }];
+            type: "line",
+            name: "5Y average"
+        };
 
         const data = [trace2020, trace5y];
 
         // Layout for line graph
         const layout = {
             title: "2020 AQI vs. 5Y average",
-            height: 600,
-            width: 980
+            height: 700,
+            width: 1000,
+            xaxis: {
+                title: "Date"
+              },
+              yaxis: {
+                title: "Air quality index",
+                tick0: 0,
+              },
+            paper_bgcolor: 'rgba(0,0,0,0)',
+            plot_bgcolor: 'rgba(0,0,0,0)'        
         };
 
         Plotly.newPlot("plot", data, layout);
